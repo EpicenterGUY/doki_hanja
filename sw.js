1d51947d1347015f7320df3d495ca5dd2fea2b53
EpicenterGUY <mintbamtol7@gmail.com>
2026-09-24 20:22:59 +0900

    Bust PWA cache for grade diagnosis 2.8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="doki-hanja-app-v11-20260924";
+const CACHE="doki-hanja-app-v12-20260924";
 const CORE=["./","./index.html","./manifest.webmanifest","./version.json","./icon.svg","./icon-maskable.svg"];
 
 self.addEventListener("install",event=>{
