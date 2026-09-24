4d505f3a5d3de17555ba7d5eef62e53a468583e8
EpicenterGUY <mintbamtol7@gmail.com>
2026-09-24 19:07:51 +0900

    Bust PWA cache for official hun-eum UI 2.4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="doki-hanja-app-v7-20260924";
+const CACHE="doki-hanja-app-v8-20260924";
 const CORE=["./","./index.html","./manifest.webmanifest","./version.json","./icon.svg","./icon-maskable.svg"];
 
 self.addEventListener("install",event=>{
