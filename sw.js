249cd2c60238932fd1b9dcf5115e0392f55e448b
EpicenterGUY <mintbamtol7@gmail.com>
2026-09-25 02:20:36 +0900

    Bust PWA cache for 2.10.7

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="doki-hanja-app-v30-20260924";
+const CACHE="doki-hanja-app-v31-20260925";
 const CORE=["./","./index.html","./manifest.webmanifest","./version.json","./icon.svg","./icon-maskable.svg","./report-config.js"];
 
 self.addEventListener("install",event=>{
