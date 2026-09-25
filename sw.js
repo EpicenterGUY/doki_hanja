12fc117607ab22833f6fe8274ecb542cef503104
EpicenterGUY <mintbamtol7@gmail.com>
2026-09-25 13:36:13 +0900

    Cache Japanese atlas and word-writing assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="doki-hanja-app-v35-20260925";
-const CORE=["./","./index.html","./manifest.webmanifest","./version.json","./icon.svg","./icon-maskable.svg","./report-config.js","./japanese.css","./japanese.js","./data/japanese/joyo.tsv","./data/japanese/hyogai.txt","./data/japanese/NOTICE.txt"];
+const CACHE="doki-hanja-app-v36-20260925";
+const CORE=["./","./index.html","./manifest.webmanifest","./version.json","./icon.svg","./icon-maskable.svg","./report-config.js","./japanese.css","./japanese.js","./data/japanese/joyo.tsv","./data/japanese/hyogai.txt","./data/japanese/NOTICE.txt","./data/japanese/words.json"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
